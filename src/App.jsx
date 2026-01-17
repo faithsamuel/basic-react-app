740f4be5c13a1f5e491265a943ee7c47e7abe473
Faith Samuel <faithsamuel365@gmail.com>
2026-01-18 00:08:44 +0100

    Created Task Component and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Task from "./Task";
 
 function App() {
+  const tasks = ["Pray", "Code", "Rest"];
+  
   return (
     <div>
       <h1>React</h1>
-      <p>Components.</p>
+      <h2>{tasks.length} Tasks</h2>
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task} title={task} />
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Task.jsx
@@ -0,0 +1,5 @@
+function Task({title}) {
+    return <li>{title}</li>;
+}
+
+export default Task;
