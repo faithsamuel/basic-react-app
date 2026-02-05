81fffd88c361eb6610011111dabeb45dbd567a61
Faith Samuel <faithsamuel365@gmail.com>
2026-02-05 23:32:43 +0100

    Created a Profile Card Component

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ProfileCard() {
+  return (
+    <div>
+        <p>Name:</p>
+        <p>Role:</p>
+        <ul>Skill list:</ul>
+        
+    </div>
+  )
+}
+
+export default ProfileCard
