7610e3b5d094800995ede2ac49b5b501d671cd78
Faith Samuel <faithsamuel365@gmail.com>
2026-02-09 21:19:34 +0100

    Added state to the ProfileCard component

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ProfileCard({name, role}) {
 
-  const skillsList = [ "Problom solving", "software development", "project management"
+  // const skillsList = [ "Problom solving", "software development", "project management"];
 
-  ]
+ const [skillsList, setSkill] = useState(["Problem solving", "Software development", "Project management"]);
+
+ const addSkill = () => {
+  setSkill([...skillsList, "Tech Sales"]);
+ };
   return (
     <div>
         <p>Name: {name}</p>
         <p>Role: {role}</p>
         <ul>Skill list: {skillsList.map(skill => <li key={skill}>{skill}</li>)}</ul>
+
+        <button onClick={addSkill}>Add Skill</button>
         
     </div>
   )
