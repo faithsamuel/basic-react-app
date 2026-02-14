401347887d21997d61b2cf0e233cd8b96f517c3a
Faith Samuel <faithsamuel365@gmail.com>
2026-02-14 21:02:11 +0100

    Added the Form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,6 +4,19 @@ function SignUp() {
   return (
     <div> 
         <h3>Sign Up Form</h3>
+        <form>
+            <input
+                type='text'
+             />
+            <input
+                type='text'
+             />
+            <button>Submit</button>
+
+            <ul>
+                <h5>Inputed Details:</h5>
+            </ul>
+        </form>
     </div>
   )
 }
