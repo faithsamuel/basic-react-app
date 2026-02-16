9beeedc64e1589ca9f38cfba505bed03b43c0bb4
Faith Samuel <faithsamuel365@gmail.com>
2026-02-16 22:00:01 +0100

    completed the SignUp Form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SignUp() {
+
+    const [form, setForm] = useState({
+        name: "",
+        email: ""
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setForm({
+            ...form,
+        [e.target.name]: e.target.value
+        });
+};
+
+
   return (
     <div> 
         <h3>Sign Up Form</h3>
-        <form>
+        <form onSubmit={handleSubmit}> 
             <input
                 type='text'
+                name="name"
+                value={form.name}
+                onChange={handleSubmit}
              />
             <input
                 type='text'
+                name="email"
+                value={form.email}
+                onChange={handleSubmit}
              />
             <button>Submit</button>
 
