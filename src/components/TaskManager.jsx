46cc436aac36454f350835aa5a7c7d5aece8e2ef
Faith Samuel <faithsamuel365@gmail.com>
2026-02-20 21:16:45 +0100

    Completed the Task Manager Component

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TaskManager() {
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!task.trim()) return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const deleteTask = (indexToDelete) => {
+    setTask(tasks.filter((_, index) => index !== indexToDelete));
+  };
+
   return (
     <div>
     <h1>Task Manager</h1>
-     <form>
+
+     <form onSubmit={handleSubmit}>
       <input 
-        type='text'
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
         placeholder='Enter a Task'
         />
 
-        <button>Submit</button>
+        <button type='submit' disabled={!task.trim()}>Add</button>
      </form>
+
+     <ul>
+      {tasks.map((item, index) => (
+        <li key={index}>
+          {item}
+          <button onClick={() => deleteTask(index)}>❌</button>
+        </li>
+      ))}
+     </ul>
+
     </div>
   )
 }
