a3de86a933f230acbf7a4da5b30a7189de4b5789
Faith Samuel <faithsamuel365@gmail.com>
2026-02-21 21:53:42 +0100

    Commented out old code from the Task Manager Component

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -48,3 +48,57 @@ function TaskManager() {
 }
 
 export default TaskManager
+
+
+// ******************Old Code********************************
+
+// import React, { useState } from 'react'
+
+// function TaskManager() {
+
+//   const [task, setTask] = useState("");
+//   const [tasks, setTasks] = useState([]);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if(!task.trim()) return;
+
+//     setTasks([...tasks, task]);
+//     setTask("");
+//   };
+
+//   const deleteTask = (indexToDelete) => {
+//     setTask(tasks.filter((_, index) => index !== indexToDelete));
+//   };
+
+//   return (
+//     <div>
+//     <h1>Task Manager</h1>
+
+//      <form onSubmit={handleSubmit}>
+//       <input 
+//         value={task}
+//         onChange={(e) => setTask(e.target.value)}
+//         placeholder='Enter a Task'
+//         />
+
+//         <button type='submit' disabled={!task.trim()}>Add</button>
+//      </form>
+
+//      {tasks.length === 0 && <p>No Tasks yet 👀🙂‍↔️👎🏾</p>}
+
+//      <ul>
+//       {tasks.map((item, index) => (
+//         <li key={index}>
+//           {item}
+//           <button onClick={() => deleteTask(index)}>❌</button>
+//         </li>
+//       ))}
+//      </ul>
+
+//     </div>
+//   )
+// }
+
+// export default TaskManager
